6380293253b03db6dddab6eaf2961566be2816cf
Walker Morgan <MrDynamoMLG@gmail.com>
2020-11-29 16:32:32 -0600

    Initial Commit

---
+++ ActivityTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActivityTracker
+//
+//  Created by Morgan, Walker J on 11/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
