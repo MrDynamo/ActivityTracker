6ca4f66a75fea4a07d8b04058a45da6a631fa9d1
Walker Morgan <MrDynamoMLG@gmail.com>
2020-11-29 18:37:38 -0600

    Started Main Screen implementation

--- ActivityTracker/ViewController.swift
+++ ActivityTracker/ViewController.swift
@@ -13,9 +13,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = UIColor.purple
+        view.backgroundColor = UIColor.lightGray
         
+        // Activity table - populate from file
+        let activityTable: UITableView = UITableView(frame: CGRect(x: 25, y: 60, width: view.frame.width - 50, height: view.frame.height - 200))
+        activityTable.allowsSelection = true
+        activityTable.isUserInteractionEnabled = true
+        view.addSubview(activityTable)
         
+        // Button to add new activity
+        let newButton: UIButton = UIButton()
+        newButton.isUserInteractionEnabled = true
+        view.addSubview(newButton)
+         
+        // Button to edit activity
+        let editButton: UIButton = UIButton()
+        editButton.isUserInteractionEnabled = true
+        view.addSubview(editButton)
+        
+        // Button to delete activity
+        let deleteButton: UIButton = UIButton()
+        deleteButton.isUserInteractionEnabled = true
+        view.addSubview(deleteButton)
+        
+        // Button to access settings menu
+        let settingsButton: UIButton = UIButton()
+        settingsButton.isUserInteractionEnabled = true
+        view.addSubview(settingsButton)
         
     }
 
